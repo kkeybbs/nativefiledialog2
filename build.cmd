bc8fbed78f6f8efa8f7a6775e1380d6aa461bb45
kkeybbs <kkeybbs@gmail.com>
2019-04-01 16:53:17 +0800

    update toolset

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
 cd build/vs2010
-msbuild /nologo /verbosity:quiet /p:Configuration=Release NativeFileDialog.sln
+msbuild /nologo /verbosity:quiet /p:Configuration=Release;PlatformToolset=v141 NativeFileDialog.sln
